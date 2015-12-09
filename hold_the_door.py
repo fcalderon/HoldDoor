45bc72c70ebee60a171f5764fdba097987b5bb46
Francisco Calderon <fjavier5152@gmail.com>
2015-12-09 17:11:51 -0500

    - Adds/improves some comments - Adds and removes some "TODOs" - Modifies 'weather' variable - Modifies 'familiar' variable

--- hold_the_door.py
+++ hold_the_door.py
@@ -1,38 +1,44 @@
 __author__ = 'Francisco Calderon and Grant Merrill'
 
-#TODO Update weather to be a number between 0 and 1
+# TODO Update attractiveness to be a number between 0 and 10
+# TODO update familiar to be whether the person is authorized to enter through that door
+
 
 doorHoldingData = [
-    {"distance": 4.5, "speed": 5, "attractiveness": 0, "familiarity": 0, "carrying": True, "weather": .9},
-    {"distance": 7.5, "speed": 4, "attractiveness": 1, "familiarity": 0, "carrying": False,
+    {"distance": 4.5, "speed": 5, "attractiveness": 0, "familiar": False, "carrying": True, "weather": .9},
+    {"distance": 7.5, "speed": 4, "attractiveness": 1, "familiar": False, "carrying": False,
      "weather": "scatter clouds, cold"},
-    {"distance": 4, "speed": 4, "attractiveness": 1, "familiarity": 0, "carrying": False, "weather": .3},
-    {"distance": 6.5, "speed": 4.2, "attractiveness": 3, "familiarity": 0, "carrying": True,
+    {"distance": 4, "speed": 4, "attractiveness": 1, "familiar": False, "carrying": False, "weather": 1},
+    {"distance": 6.5, "speed": 4.2, "attractiveness": 3, "familiar": True, "carrying": True,
      "weather": "snowing, very cold, icy"},
-    {"distance": 3, "speed": 4.5, "attractiveness": 0, "familiarity": 0, "carrying": False, "weather": "raining, warm"},
-    {"distance": 7, "speed": 3, "attractiveness": 0, "familiarity": 0, "carrying": True, "weather": "snowing"},
-    {"distance": 3, "speed": 4.5, "attractiveness": 0, "familiarity": 0, "carrying": False, "weather": "sunny, very cold"},
-    {"distance": 15, "speed": 5, "attractiveness": 3, "familiarity": 0, "carrying": True, "weather": "cloudy, warm"},
-    {"distance": 3, "speed": 4.5, "attractiveness": 0, "familiarity": 0, "carrying": True, "weather": "cloudy, cold"},
-    {"distance": 20, "speed": 3, "attractiveness": 0, "familiarity": 1, "carrying": False, "weather": "sunny, warm"},
-    {"distance": 25, "speed": 3, "attractiveness": 0, "familiarity": 0, "carrying": False,
-     "weather": "raining, thunderstorm, warm"},
-    {"distance": 20, "speed": 3, "attractiveness": 0, "familiarity": 0, "carrying": True, "weather": "raining, cold"}
+    {"distance": 3, "speed": 4.5, "attractiveness": 0, "familiar": False, "carrying": False, "weather": .5},
+    {"distance": 7, "speed": 3, "attractiveness": 0, "familiar": False, "carrying": True, "weather": .3},
+    {"distance": 3, "speed": 4.5, "attractiveness": 0, "familiar": False, "carrying": False, "weather": .7},
+    {"distance": 15, "speed": 5, "attractiveness": 3, "familiar": False, "carrying": True, "weather": .6},
+    {"distance": 3, "speed": 4.5, "attractiveness": 0, "familiar": False, "carrying": True, "weather": .7},
+    {"distance": 20, "speed": 3, "attractiveness": 0, "familiar": True, "carrying": False, "weather": 1},
+    {"distance": 25, "speed": 3, "attractiveness": 0, "familiar": False, "carrying": False,
+     "weather": .6},
+    {"distance": 20, "speed": 3, "attractiveness": 0, "familiar": True, "carrying": True, "weather": .5}
 ]
 
+# Mean time it takes to fully open a door
 TIME_TO_OPEN_DOOR = 1
+
+# The general area everyone is entitled to
 GENERAL_GRAY_AREA = 2
 
-WEATHER_STATES = ['COLD', 'VERY COLD', 'RAINING', 'SNOWING', ]
+# The gray area added if someone is familiar
+FAMILIARITY_GRAY_AREA = 4
 
 
 def gray_area_size(attractiveness, weather=1, carrying=False):
     """
     Determines the size of the gray area given to a certain person
     :param attractiveness: Number between 0 and 10 describing the attractiveness of a person
-    :param weather: what the weather is between 0 (very bad) and 1 (very nice)
+    :param weather: what the weather is between 0 (very bad) and 1 (very nice or indoor)
     :param carrying: whether the person is holding something
-    :return:
+    :return: The gray area
     """
     g_area = GENERAL_GRAY_AREA * weather
     if carrying:
@@ -46,12 +52,11 @@ def gray_area_size(attractiveness, weather=1, carrying=False):
 
 def hold_door(is_someone_behind_you, data=None):
     """
-
+    Returns whether the door should be held
     :param is_someone_behind_you: whether there's a person for whom the door could be held for
-    :param data: the data
-    :return:
+    :param data: the data to determine whether the hold should be held
+    :return: True if the door should be held, False otherwise
     """
-
     if is_someone_behind_you and data:
         if data["distance"] <= 4.5:
             return True
@@ -60,5 +65,8 @@ def hold_door(is_someone_behind_you, data=None):
         elif (data["distance"] - 4.5) < data["speed"] - TIME_TO_OPEN_DOOR:
             return True
 
-    return False
-
+        return False
+    elif is_someone_behind_you:
+        raise NameError('If \'is_someone_behind_you\' is true then \'data\' must be provided')
+    else:
+        return True
